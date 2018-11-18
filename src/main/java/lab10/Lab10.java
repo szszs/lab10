ba7b8ed93abf8abadb6bb727655cbdbac68423d4
szszs <shawn.zhou2@gmail.com>
2018-11-18 15:51:10 -0800

    Implemented task

--- src/main/java/lab10/Lab10.java
+++ src/main/java/lab10/Lab10.java
@@ -2,5 +2,21 @@ package lab10;
 
 import com.google.gson.*;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Lab10 {
+    public static void main(String args[]) {
+        String filename = "datasets/products.json";
+        JsonParser parser = new JsonParser();
+        try(FileReader fr = new FileReader(filename)) {
+            JsonElement json = parser.parse(fr);
+            Gson g = new Gson();
+            Product[] p = g.fromJson(json, Product[].class);
+            System.out.println(p);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 }
+
