3db08b5c09c47fbc4fc642c2d8bdfc8ea0762250
szszs <shawn.zhou2@gmail.com>
2018-11-18 16:01:39 -0800

    Implemented task

---
+++ src/main/java/lab10/Product.java
@@ -0,0 +1,9 @@
+package lab10;
+
+public class Product {
+    public String product_name;
+    public String supplier;
+    public int quantity;
+    public String unit_cost;
+    public ProductID _id;
+}
